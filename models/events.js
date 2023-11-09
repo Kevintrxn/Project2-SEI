7b05323c9a2a38d0cff3edaa6f573b14aca8a087
Kevintrxn <kevintran12kt@gmail.com>
2023-11-09 02:50:28 -0600

    initial commit

---
+++ models/events.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentsSchema = new Schema({
+    comment: {
+        type: String,
+        required: true
+},
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+}
+}, {
+    timestamps: true 
+});
+
+const eventSchema = new Schema({
+    eventName: {
+        type: String,
+        required: true
+},
+    eventLocation: {
+        type: String,
+        required: true
+},
+    eventDate: {
+        type: Date,
+        required: true
+},
+    experience: {
+        type: String,
+        enum: ['positive', 'negative'],
+        required: true
+},
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+},
+    reviewText: {
+        type: String,
+        required: true
+},
+    comments: [commentsSchema] 
+}, {
+    timestamps: true 
+});
+
+module.exports = mongoose.model('Event', eventSchema); 
