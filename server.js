5cd3b18dbca8feeee1aef17917d9ce410e933caf
Kevintrxn <kevintran12kt@gmail.com>
2023-11-09 04:43:10 -0600

    routes/ctrl/models updated

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+require('dotenv').config();
+require('./config/database');
+
+// Routers 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var eventsRouter = require('./routes/events');
 
 var app = express();
 
@@ -17,9 +23,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Using router paths 
+app.use('/events', eventsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
