a5c0ba935a588a70f6b8027a2ec245b753a92552
Kevintrxn <kevintran12kt@gmail.com>
2023-11-10 02:49:25 -0600

    Fixed user log out

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,15 @@ const eventsController = require('../controllers/events')
 
 
 router.get('/', eventsController.listEvents);
-// Google OAuth login route
+
 router.get('/auth/google', passport.authenticate(
-    // Which passport strategy is being used?
     'google',
     {
-      // Requesting the user's profile and email
     scope: ['profile', 'email'],
-      // Optionally force pick account every time
-      // prompt: "select_account"
     }
 ));
 
-// Google OAuth callback route
+
 router.get('/oauth2callback', passport.authenticate(
     'google',
     {
@@ -27,7 +23,7 @@ router.get('/oauth2callback', passport.authenticate(
     }
 ));
 
-  // OAuth logout route
+
 router.get('/logout', function(req, res){
     req.logout(function() {
     res.redirect('/');
